e8edd3ca4c2d9bd0c5042dc079b24eeec38982cf
tasutil <tasutil@unirioja.es>
2022-02-28 23:03:49 +0100

    test

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -14,4 +14,11 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun generates_number() {
+        val dice = MainActivity.Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+    }
 }
